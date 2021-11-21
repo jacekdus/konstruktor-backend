780c2314ee5ac8e0c8036a962bd1025d59a6b14b
Jacek Duś <jacekdus1990@gmail.com>
2021-11-21 21:43:23 +0100

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return phpinfo();
+});
+
+$router->get('db_test', function () {
+    return app('db')->select("SELECT * FROM Persons");
+});
+
+$router->get('hello', function () {
+    return 'hello World!';
+});
